2ec7601c3cee6db9898b4f7898c9c0e13a7c1849
André Ramos <web.andreramos@gmail.com>
2024-01-24 16:01:59 -0300

    expressoes js no jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+const post = "Posts da semana..";
 
 function App(){
-  return <h1>Componente App</h1>
+  return (
+   <> 
+      <h1>Blog</h1>
+      <h1>Subtítulo</h1>
+      <h3>{post}</h3>
+   </>
+  )
 }
 
 export default App;
