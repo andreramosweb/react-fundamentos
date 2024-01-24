f217b41730bb38800b74e1d924afdce5e86e4355
André Ramos <web.andreramos@gmail.com>
2024-01-24 15:29:12 -0300

    configuracao do webpack server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import  ReactDOM  from "react-dom";
 
 ReactDOM.render(
   <h1 id="title">
-    <span>Olá Mundo, Hello World!</span>
+    <span>Olá Mundo, Hello World!!!</span>
   </h1>,
   document.getElementById('root'),
 );
